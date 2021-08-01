05162c8c8063f11bd8fad9ce4c70b7aed83dbdeb
Malachi Soord <inverse.chi@gmail.com>
2021-08-01 22:31:12 +0200

    Tidy (#32)
    * Tidy
    
    * Update Scraper.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,9 +9,11 @@ return PhpCsFixer\Config::create()
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
+        '@PSR12' => true,
         '@PhpCsFixer' => true,
         'php_unit_test_class_requires_covers' => false,
         'php_unit_internal_class' => false,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
     ;
