3fa187bd25983ce39a09769e962f928c246ae74f
Malachi Soord <inverse.chi@gmail.com>
2022-09-08 11:50:46 +0200

    Improve AfterRule

--- tests/Config/Rules/AfterRuleTest.php
+++ tests/Config/Rules/AfterRuleTest.php
@@ -6,12 +6,20 @@ namespace Tests\Inverse\Termin\Config\Rules;
 
 use DateInterval;
 use DateTime;
+use InvalidArgumentException;
 use Inverse\Termin\Config\Rules\AfterRule;
 use Inverse\Termin\Result;
 use PHPUnit\Framework\TestCase;
 
 class AfterRuleTest extends TestCase
 {
+    public function testInvalidInput(): void
+    {
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage('Failed to parse input: Exception: Unknown or bad format (HelloWorld)'); 
+        new AfterRule('HelloWorld');
+    }
+
     public function testPassesValid(): void
     {
         $rule = new AfterRule('PT24H');
