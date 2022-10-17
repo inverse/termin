5ac55e4f4f1475a274745f0128010fad4bb118b1
Malachi Soord <inverse.chi@gmail.com>
2022-10-17 08:51:12 +0200

    Fix up

--- src/Notifier/NtfyNotifier.php
+++ src/Notifier/NtfyNotifier.php
@@ -23,7 +23,7 @@ class NtfyNotifier implements NotifierInterface
     public function notify(string $label, string $url, DateTime $date): void
     {
         $body = sprintf('%s appointment found for %s', $label, $date->format('jS M Y'));
-        $message = new Message($this->server);
+        $message = new Message();
         $message->topic($this->topic);
         $message->title('Appointment Found');
         $message->body($body);
