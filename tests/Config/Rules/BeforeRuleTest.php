305cf944851804d8890d8af8f2a1eb4c60a232f4
Malachi Soord <inverse.chi@gmail.com>
2023-11-27 21:49:33 +0100

    Bump dependencies (#87)
    * Bump dependencies
    
    * Fix up
    
    * Fix up

--- tests/Config/Rules/BeforeRuleTest.php
+++ tests/Config/Rules/BeforeRuleTest.php
@@ -14,7 +14,7 @@ class BeforeRuleTest extends TestCase
     {
         self::expectException(\InvalidArgumentException::class);
         new BeforeRule('HelloWorld');
-        self::assertStringStartsWith('Failed to parse before', self::getExpectedExceptionMessage());
+        self::expectExceptionMessage('Failed to parse before');
     }
 
     public function testPassesValid(): void
